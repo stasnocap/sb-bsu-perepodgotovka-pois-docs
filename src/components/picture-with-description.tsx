79f94f7303bc770713cecbbf787e67575828cba7
Stas <stasnocap@gmail.com>
2023-11-25 12:12:25 +0300

    add OAIP algorithms-and-programs page

---
+++ src/components/picture-with-description.tsx
@@ -0,0 +1,12 @@
+﻿export const PictureWithDescription = ({ imgSrc, description }: { imgSrc: string, description: string }) => {
+    const imgAlt = imgSrc.substring(imgSrc.lastIndexOf("/") + 1)
+    return (
+        <div className="flex items-center mt-4">
+            <img height="200" width="200" src={imgSrc} alt={imgAlt} />
+            <span className="me-2">
+                —
+            </span>
+            <span className="text-justify" dangerouslySetInnerHTML={{__html: description}}></span>
+        </div>
+    )
+}
---
+++ src/pages/_app.tsx
@@ -0,0 +1,7 @@
+﻿import "./globals.css"
+
+function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps} />
+}
+
+export default MyApp
